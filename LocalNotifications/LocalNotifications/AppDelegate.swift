16c189e177441446e090ee4d65f93bec420c48bb
TLiLi HAMDI <tlilios@yahoo.com>
2024-05-21 19:26:33 +0200

    End

--- LocalNotifications/LocalNotifications/AppDelegate.swift
+++ LocalNotifications/LocalNotifications/AppDelegate.swift
@@ -8,13 +8,23 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
         return true
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .badge, .sound])
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("Notification Clicked!!!")
+        if let dictionary = response.notification.request.content.userInfo as? [String: Any] {
+            print("Dictionary: \(dictionary)")
+            print("Name: \(dictionary["name"] ?? "Default value")")
+        }
     }
 
     // MARK: UISceneSession Lifecycle
--- LocalNotifications/LocalNotifications/ViewController.swift
+++ LocalNotifications/LocalNotifications/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func btnRemoveNotification(_ sender: UIButton) {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["TestID"])
+    }
+    
+    @IBAction func btnTestNotification(_ sender: UIButton) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if granted {
+                print("granted")
+                DispatchQueue.main.sync {
+                    self.scheduleNotification()
+                }
+                
+            } else {
+                print("denied")
+            }
+        }
+    }
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Title"
+        content.subtitle = "SubTitle"
+        content.body = "Notification"
+        content.badge = 1
+        content.sound = .default
+        content.userInfo = ["name": "My test name"]
+        
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(5), repeats: false)
+//        var component = DateComponents()
+//        component.calendar = Calendar.current
+//        component.weekday = 1
+//        component.hour = 21
+//        component.minute = 30
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: component, repeats: false)
+        let date = datePicker.date
+        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date), repeats: false)
+        let request = UNNotificationRequest(identifier: "TestID", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+    }
+    
 }
 
